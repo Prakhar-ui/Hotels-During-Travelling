53c09e4ef5ff9a60620c7bf095ced900811fbb17
Prakhar-ui <prakha8380@gmail.com>
2023-04-12 22:47:26 +0530

    Initial Commit

---
+++ src/main/java/com/example/HotelsDuringTravelling/HotelsDuringTravellingApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelsDuringTravelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsDuringTravellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsDuringTravellingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HotelsDuringTravelling/HotelsDuringTravellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelsDuringTravelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsDuringTravellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
